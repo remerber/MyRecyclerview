3014ed0f5c9194f933795dbb29a7e5f22e6f0af4
Remeber <275281462@qq.com>
2017-02-20 16:09:33 +0800

    最基本的Recyclerview的写法

--- app/src/main/java/com/example/administrator/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/administrator/myrecyclerview/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.administrator.myrecyclerview;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.administrator.myrecyclerview.core.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends BaseActivity implements SwipeRefreshLayout.OnRefreshListener {
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private RecyclerView mRecyclerView;
+    private SampleListAdapter adapter;
+    private ArrayList<String> mDataList = new ArrayList<>();
 
     @Override
     protected void setUpContentView() {
-        setContentView(R.layout.activity_main,R.string.list);
+        setContentView(R.layout.activity_main, R.string.list);
     }
 
     @Override
     protected void setUpView() {
-
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
 
     @Override
     protected void setUpData(Bundle savedInstanceState) {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        adapter = new SampleListAdapter();
+        mRecyclerView.setAdapter(adapter);
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                onRefresh();
+            }
+        });
+    }
+
+    @Override
+    public void onRefresh() {
+        mDataList.clear();
+        for (int i = 0; i < 50; i++) {
+            mDataList.add("list item is" + i);
+        }
+        adapter.notifyDataSetChanged();
+        mSwipeRefreshLayout.setRefreshing(false);
+
+    }
+
+    class SampleListAdapter extends RecyclerView.Adapter<SampleViewHolder> {
+
+        @Override
+        public SampleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_sample_list_item, parent, false);
+            return new SampleViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(SampleViewHolder holder, int position) {
+            holder.mSampleListItemLabel.setText(mDataList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataList.size();
+        }
+    }
+
+    class SampleViewHolder extends RecyclerView.ViewHolder {
+        TextView mSampleListItemLabel;
+
+        public SampleViewHolder(View itemView) {
+            super(itemView);
+            mSampleListItemLabel = (TextView) itemView.findViewById(R.id.mSampleListItemLabel);
+        }
 
     }
 }
